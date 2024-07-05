08e1e062d55bdd752744dfce30f7aae35adb31e2
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2024-07-05 16:48:00 +0200

    [MMC-12] Added scheduler for queue

--- src/main/java/it/gov/pagopa/message/core/event/producer/MessageErrorProducer.java
+++ src/main/java/it/gov/pagopa/message/core/event/producer/MessageErrorProducer.java
@@ -7,6 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @Component
 @Slf4j
 public class MessageErrorProducer {
@@ -15,7 +19,7 @@ public class MessageErrorProducer {
   private final String binder;
 
   private final StreamBridge streamBridge;
-
+  private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
   public MessageErrorProducer( @Value("${spring.cloud.stream.bindings.messageSender-out-0.binder}")String messageErrorOut0,
                                StreamBridge streamBridge) {
     this.binder = messageErrorOut0;
@@ -23,9 +27,12 @@ public class MessageErrorProducer {
   }
 
   public void sendToMessageErrorQueue(Message<MessageDTO> message){
-    log.debug("Sending messsage to queue");
-    streamBridge.send("messageSender-out-0",binder, message);
-  }
-
-
+    log.debug("Scheduling message to queue");
+      scheduler.schedule(() -> {
+        streamBridge.send("messageSender-out-0", binder, message);
+      }, 5, TimeUnit.SECONDS);
+    }
 }
+
+
+
