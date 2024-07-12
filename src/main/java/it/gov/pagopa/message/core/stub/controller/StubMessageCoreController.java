b06297e719123a7224444ec5b6fe43fa69581410
stefanodel <stefano11.delia@nttdata.com>
2024-07-12 14:37:46 +0200

    fix: add crossorigin for cors policy

--- src/main/java/it/gov/pagopa/message/core/stub/controller/StubMessageCoreController.java
+++ src/main/java/it/gov/pagopa/message/core/stub/controller/StubMessageCoreController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
-@CrossOrigin
 @RequestMapping("stub/emd/message-core")
 public interface StubMessageCoreController {
 
