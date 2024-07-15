61d571d42c9c013ca97dffbbced2f953936580c9
Vitolo-Andrea <157486351+Vitolo-Andrea@users.noreply.github.com>
2024-07-15 15:02:08 +0200

    fix: Frontend File Position Change (#9)

--- src/main/java/it/gov/pagopa/message/core/stub/controller/ReactController.java
+++ src/main/java/it/gov/pagopa/message/core/stub/controller/ReactController.java
@@ -1,4 +1,4 @@
-package it.gov.pagopa.message.core.controller;
+package it.gov.pagopa.message.core.stub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
