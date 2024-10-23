1298d7c7d7de23b0534cc645392d54c1736a8e1d
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2024-10-23 15:13:16 +0200

    fix-pom

--- src/test/java/it/gov/pagopa/message/controller/MessageCoreControllerTest.java
+++ src/test/java/it/gov/pagopa/message/controller/MessageCoreControllerTest.java
@@ -12,12 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
 @WebFluxTest(MessageCoreControllerImpl.class)
-@ContextConfiguration(classes = MessageCoreControllerImpl.class)
 class MessageCoreControllerTest {
 
     @MockBean
