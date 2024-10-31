8e2f39de3e13ac311471e7eb7de93110307109c3
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2024-10-31 16:32:53 +0100

    Queue header update

--- src/main/java/it/gov/pagopa/message/service/MessageProducerServiceImpl.java
+++ src/main/java/it/gov/pagopa/message/service/MessageProducerServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
+import static it.gov.pagopa.message.constants.MessageCoreConstants.MessageHeader.ERROR_MSG_HEADER_RETRY;
+
 @Slf4j
 @Service
 public class MessageProducerServiceImpl implements MessageProducerService {
@@ -29,6 +31,7 @@ public class MessageProducerServiceImpl implements MessageProducerService {
     private static Message<MessageDTO> createMessage(MessageDTO messageDTO) {
         return MessageBuilder
                 .withPayload(messageDTO)
+                .setHeader(ERROR_MSG_HEADER_RETRY, 0L)
                 .build();
 
     }
