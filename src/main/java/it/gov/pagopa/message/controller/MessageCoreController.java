fd767f5b950927e3b5a6614ac0f8b81885e2aa26
fabio.picano <fabio.picano@nttdata.com>
2026-01-21 12:18:38 +0100

    Add springdoc dependency

--- src/main/java/it/gov/pagopa/message/controller/MessageCoreController.java
+++ src/main/java/it/gov/pagopa/message/controller/MessageCoreController.java
@@ -10,6 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import reactor.core.publisher.Mono;
 
 /**
@@ -17,6 +24,10 @@ import reactor.core.publisher.Mono;
  *
  * <p>Error semantics and domain flows are documented in the service layer; controller focuses on HTTP contract.</p>
  */
+@Tag(
+    name = "Notification Management", 
+    description = "API per la gestione delle notifiche."
+)
 @RequestMapping("/emd/message-core")
 public interface MessageCoreController {
 
@@ -32,6 +43,18 @@ public interface MessageCoreController {
      *          <li> 202 Accepted with {@code "NO_CHANNELS_ENABLED"} if recipient has no active channels</li>
      *        </ul>
      */
+    @Operation(
+        summary = "Starts the process to deliver the notification of the message to the TPP",
+        description = "Starts the process to deliver the notification of the message to the TPP."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully queued",
+            content = @Content(schema = @Schema(implementation = SendResponseDTO.class))),
+        @ApiResponse(responseCode = "202", description = "No channels enabled",
+            content = @Content(schema = @Schema(implementation = SendResponseDTO.class))),
+        @ApiResponse(responseCode = "400", description = "Error in the request syntax or semantics",
+            content = @Content),
+    })
     @PostMapping("/sendMessage")
     Mono<ResponseEntity<SendResponseDTO>> send(@Valid @RequestBody MessageDTO messageDTO);
 }
